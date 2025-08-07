d1486c30cb05a70c8cb14441561c22118ec20290
DarshanDS004 <1si23is020@sit.ac.in>
2025-08-07 11:50:09 +0530

    Time: 800 ms (5.08%), Space: 26.2 MB (70.94%) - LeetHub

--- 0978-valid-mountain-array/0978-valid-mountain-array.cpp
+++ 0978-valid-mountain-array/0978-valid-mountain-array.cpp
@@ -1,3 +1,41 @@
+
+
+class Solution {
+public:
+    bool validMountainArray(vector<int>& arr) {
+        int n = arr.size();
+        if (n < 3) return false;
+
+        for (int i = 1; i < n - 1; i++) {
+            bool increasing = true;
+            bool decreasing = true;
+
+            // Check increasing before peak
+            for (int j = 0; j < i; j++) {
+                if (arr[j] >= arr[j + 1]) {
+                    increasing = false;
+                    break;
+                }
+            }
+
+            // Check decreasing after peak
+            for (int j = i; j< n - 1; j++) {
+                if (arr[j] <= arr[j + 1]) {
+                    decreasing = false;
+                    break;
+                }
+            }
+
+            if (increasing && decreasing)
+             return true;
+        }
+
+        return false;
+    }
+};
+
+
+/*
 class Solution {
 public:
     bool validMountainArray(vector<int>& arr) {
@@ -20,3 +58,4 @@ public:
         
     }
 };
+*/
