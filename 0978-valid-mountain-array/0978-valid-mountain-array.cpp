b4568752584e3721f0e7e20abd1eae8d98dc9ba8
DarshanDS004 <1si23is020@sit.ac.in>
2025-08-07 11:44:14 +0530

    Time: 25 ms (12.89%), Space: 26.3 MB (41.14%) - LeetHub

--- 0978-valid-mountain-array/0978-valid-mountain-array.cpp
+++ 0978-valid-mountain-array/0978-valid-mountain-array.cpp
@@ -5,13 +5,13 @@ public:
         if(n<3)
         return false;
         int i=0;
-        while(i<n && arr[i]<arr[i+1])
+        while(+1<n && arr[i]<arr[i+1])
         {
             i++;
         }
         if(i==0||i==n-1)
         return false;
-        while(i<n-1 && arr[i]>arr[i+1])
+        while(i+1<n && arr[i]>arr[i+1])
         {
             i++;
         }
