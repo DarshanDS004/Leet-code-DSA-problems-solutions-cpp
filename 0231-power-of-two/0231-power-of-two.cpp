5acef9ece39a7892bdcbb334a4b9dca0a163f444
DarshanDS004 <1si23is020@sit.ac.in>
2025-08-09 20:50:57 +0530

    Time: 0 ms (100%), Space: 7.9 MB (17.71%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,23 +1,16 @@
-/*
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
         if(n<=0)
-        return  false;
+        return false;
         while(n%2==0)
         {
-          n/=2;
-            
+            n/=2;
         }
         if(n==1)
         return true;
+        else
         return false;
-    }
-};
-*/
-class Solution {
-public:
-    bool isPowerOfTwo(int n)  {
-        return n>0 &&!( n & n-1);     // or  return n>0 && not( n & n-1);      
+        
     }
 };
