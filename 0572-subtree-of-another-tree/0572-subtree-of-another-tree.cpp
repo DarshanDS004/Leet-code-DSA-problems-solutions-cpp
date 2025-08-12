c1cf344d381e4b66dafe9be53cfba6d8da6a0891
DarshanDS004 <1si23is020@sit.ac.in>
2025-08-12 12:03:47 +0530

    Time: 20 ms (5.17%), Space: 38.5 MB (5.02%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -9,6 +9,9 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+ /*
+
 class Solution {
 public:
 string preorder(TreeNode*root)
@@ -30,4 +33,51 @@ string preorder(TreeNode*root)
         return main.find(sub)!=string::npos;
         
     }
+};
+
+*/
+
+
+class Solution {
+public:
+string preorder(TreeNode*root)
+{
+      string res="";
+    if(root==nullptr)
+    return "#";
+  
+    res="@"+to_string(root->val);
+    string left= preorder(root->left);
+    string right=preorder(root->right);
+    res+=left;
+    res+=right;
+    return res;
+}
+
+int string_match(string mainStr, string sub) {
+    int m = mainStr.size();
+    int n = sub.size();
+    int ans = -1;
+
+    if (n == 0 || m < n) return -1;
+
+    for (int i = 0; i <= m - n; i++) {      // include m-n
+        int j = 0;
+        while (j < n && mainStr[i + j] == sub[j]) {
+            j++;
+        }
+        if (j == n) {  // full match
+            ans = i;
+            break;     // stop at first match
+        }
+    }
+    return ans;
+}
+
+ bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+    string main=preorder(root);
+    string sub=preorder(subRoot);
+    int res=string_match(main ,sub);
+  return res!=-1;                                                                   
+ }
 };
