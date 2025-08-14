b6158a04ecb627c411d6c300b8834f3b0d4b1b06
DarshanDS004 <1si23is020@sit.ac.in>
2025-08-14 11:34:18 +0530

    Time: 4 ms (3.58%), Space: 16.8 MB (5.35%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
@@ -8,6 +8,8 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+ /*
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
@@ -33,5 +35,73 @@ public:
         return head;
     
         
+    }
+};
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+ /*
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(head==nullptr||head->next==nullptr)
+        return head;
+        ListNode*temp=head;
+        ListNode*res=head;
+        map<int,pair<int,ListNode*>>mp;
+        mp[temp->val].first++;
+        mp[temp->val].second=temp;
+        temp=temp->next;
+        while(temp!=nullptr)
+        {
+            if(mp[temp->val].first>=1)
+            {
+             
+              res->next=mp[temp->val].second;
+              res=res->next;
+              temp=temp->next;
+
+            }
+             mp[temp->val].first++;
+             mp[temp->val].second=temp;
+            //temp=temp->next;
+        }
+        res->next=nullptr;
+        return head;
+    
+        
+    }
+};
+
+*/
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (!head) return head;
+
+        map<int, bool> seen;
+        ListNode* curr = head;
+        seen[curr->val] = true;
+
+        while (curr->next) {
+            if (seen[curr->next->val]) {
+                // Duplicate -> skip it
+                curr->next = curr->next->next;
+            } else {
+                seen[curr->next->val] = true;
+                curr = curr->next;
+            }
+        }
+        return head;
     }
 };
