4473d8811584ffbc68e2a02bf4967005939425e2
DarshanDS004 <1si23is020@sit.ac.in>
2025-08-15 20:33:48 +0530

    Time: 0 ms (100%), Space: 7.9 MB (52.41%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,12 +1,17 @@
+
 class Solution {
 public:
     bool isPowerOfFour(int n) {
         if(n<=0)
-        return false;
+        return 0;
         while(n%4==0)
         {
             n/=4;
         }
-        return n==1;
+        if(n==1)
+        return true;
+        return false;
     }
 };
+
+
