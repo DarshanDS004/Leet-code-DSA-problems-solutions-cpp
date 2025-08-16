ba0cce84ee9f1abce7bdc42912da370b4ddf6c31
DarshanDS004 <1si23is020@sit.ac.in>
2025-08-16 09:43:11 +0530

    Time: 13 ms (21.38%), Space: 87.7 MB (43.54%) - LeetHub

--- 1427-all-elements-in-two-binary-search-trees/1427-all-elements-in-two-binary-search-trees.cpp
+++ 1427-all-elements-in-two-binary-search-trees/1427-all-elements-in-two-binary-search-trees.cpp
@@ -9,6 +9,11 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+
+
+/*
+ //Brute Force approch
 class Solution {
 public:
 
@@ -65,3 +70,68 @@ public:
         
     }
 };
+
+*/
+
+
+class Solution {
+public:
+
+  /*vector<int>merge(vector<int>&a1,vector<int>&a2)
+  {
+    vector<int>res(a1.size()+a2.size());
+    int i=0,j=0,k=0;
+
+    while(i<a1.size()&&j<a2.size())
+    {
+        if(a1[i]<=a2[j])
+        {
+            res[k++]=a1[i++];
+        }
+        else
+        {
+            res[k++]=a2[j++];
+        }
+    }
+
+    while(i<a1.size())
+    {
+        res[k++]=a1[i++];
+    }
+    
+    while(j<a2.size())
+    {
+        res[k++]=a2[j++];
+    }
+
+    return res;
+  }
+  */
+
+ void inorder(TreeNode*root,vector<int>&res)
+ {
+    if(root==nullptr)
+    return;
+    inorder(root->left,res);
+    res.push_back(root->val);
+    inorder(root->right,res);
+ }
+    vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
+        if(root1==nullptr&&root2== nullptr)
+        return{};
+      
+        vector<int>t1,t2;
+        inorder(root1,t1);
+        inorder(root2,t2);
+        int m=t1.size();
+        int n=t2.size();
+        vector<int>res(t1.begin(),t1.end());
+        res.insert(res.end(),t2.begin(),t2.end());
+        sort(res.begin(),res.end());
+        return res;
+        
+        
+    }
+};
+
+
