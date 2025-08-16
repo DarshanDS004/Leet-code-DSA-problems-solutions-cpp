2f747954c068c174993353a3c65e34a88d6d3b65
DarshanDS004 <1si23is020@sit.ac.in>
2025-08-16 22:29:28 +0530

    Time: 0 ms (100%), Space: 8 MB (41.82%) - LeetHub

--- 1448-maximum-69-number/1448-maximum-69-number.cpp
+++ 1448-maximum-69-number/1448-maximum-69-number.cpp
@@ -1,7 +1,8 @@
+/*
 class Solution {
 public:
     int maximum69Number (int num) {
-        string s=to_string(num);
+      /*  string s=to_string(num);
         for(char &ch:s)
         {
             if(ch=='6')
@@ -12,5 +13,28 @@ public:
         }
         return stoi(s);
         
+    } 
+};
+*/
+
+class Solution {
+public:
+    int maximum69Number(int num) {
+        int n = num;
+        int i = 1;
+        int rev = 0;
+
+        while (n > 0) {
+            rev = rev * 10 + n % 10;
+            n /= 10;
+            i *= 10;
+        }
+
+        while (rev % 10 == 9) {
+            rev /= 10;
+            i /= 10;
+        }
+
+        return num + 3 * (i / 10);
     }
 };
