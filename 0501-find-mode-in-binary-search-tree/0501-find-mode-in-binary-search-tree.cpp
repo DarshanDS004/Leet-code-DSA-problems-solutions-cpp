ecfe3eedabcb662cae69ba6628313fc3a3ea4ebb
DarshanDS004 <1si23is020@sit.ac.in>
2025-08-19 06:55:08 +0530

    Time: 0 ms (100%), Space: 24.1 MB (98.58%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     vector<int> findMode(TreeNode* root) {
@@ -15,8 +16,10 @@ public:
             mp[curr->val]++;
             max_frq = max(max_frq, mp[curr->val]);
 
-            if (curr->left) st.push(curr->left);
-            if (curr->right) st.push(curr->right);
+            if (curr->left)
+            st.push(curr->left);
+            if (curr->right) 
+            st.push(curr->right);
         }
 
         vector<int> res;
@@ -25,6 +28,49 @@ public:
                 res.push_back(p.first);
             }
         }
+        return res;
+    }
+};
+
+*/
+
+class Solution {
+public:
+    vector<int> res;
+    int curr_ele;
+    int curr_count;
+    int max_count;
+
+    void inorder(TreeNode* root) {
+        if (!root) return;
+
+        inorder(root->left);
+
+        if (root->val == curr_ele) {
+            curr_count++;
+        } else {
+            curr_ele = root->val;
+            curr_count = 1;
+        }
+
+        if (curr_count > max_count) {
+            res.clear();
+            res.push_back(root->val);
+            max_count = curr_count;
+        } else if (curr_count == max_count) {
+            res.push_back(root->val);
+        }
+
+        inorder(root->right);
+    }
+
+    vector<int> findMode(TreeNode* root) {
+        res.clear();
+        curr_ele = INT_MIN;
+        curr_count = 0;
+        max_count = 0;
+
+        inorder(root);
         return res;
     }
 };
