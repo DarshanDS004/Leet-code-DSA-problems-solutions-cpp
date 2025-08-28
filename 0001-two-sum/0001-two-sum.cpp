5a66984a897db9844ccc54d53c898aac4ab29b4b
DarshanDS004 <1si23is020@sit.ac.in>
2025-08-28 12:52:08 +0530

    Time: 6 ms (47.42%), Space: 14.8 MB (36.14%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,20 +1,57 @@
+/*
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(nums[i]+nums[j]==target)
+                return{ i,j};
+            }
+        }
+        return  { };
+        
+    }
+};
+*/
+
+/*
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int,int>res;
         for(int i=0;i<nums.size();i++)
         {
-            int curr=nums[i];
-            int req=target-curr;
-            {
-            if(res.find(req)!=res.end())
-            return{res[req],i};
-            }
-        res[curr]=i;
+            res[nums[i]]=i;
         }
-        
-    
-    return {-1,-1};
+        for(int i=0;i<nums.size();i++)
+        {
+            int complement=target-nums[i];
+            if(res.find(complement)!=res.end()&&res[complement]!=i)
+            return{i,res[complement]};
+        }
+        return {};
     }
+};
 
+*/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int>res;
+        for(int i=0;i<nums.size();i++)
+        {
+            int complement=target-nums[i];
+            if(res.find(complement)!=res.end())
+            {
+                return { res[complement],i};
+            }
+            res[nums[i]]=i;
+        }
+        return { };
+    }
 };
