f0f88444c4435cf53df34e4a66feb50182ea8d80
DarshanDS004 <1si23is020@sit.ac.in>
2025-09-10 21:14:52 +0530

    Time: 0 ms (100%), Space: 24.3 MB (89.91%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -9,6 +9,8 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+ /*
 class Solution {
 public:
  void inorder(TreeNode*root,vector<int>&ans)
@@ -25,4 +27,30 @@ public:
         return res[k-1];
         
     }
+};
+*/
+
+class Solution {
+public:
+int count=0;
+int ans=-1;
+ void inorder(TreeNode*root,int k)
+ {
+    if(!root||ans!=-1)
+    return;
+    inorder(root->left,k);
+    count++;
+    if(count==k)
+    {
+        ans=root->val;
+        return;
+
+    }
+    inorder(root->right,k);
+ }
+
+ int kthSmallest(TreeNode* root, int k) {
+    inorder(root,k);
+    return ans;
+ }
 };
