c02651e85270a56809ebf69d722afde33724fc18
DarshanDS004 <1si23is020@sit.ac.in>
2025-09-13 06:38:18 +0530

    Time: 3 ms (21.45%), Space: 16 MB (55.35%) - LeetHub

--- 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.cpp
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.cpp
@@ -9,13 +9,11 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
- /*
 class Solution {
 public:
     vector<vector<int>> levelOrderBottom(TreeNode* root) {
         vector<vector<int>>res;
-        if(root==nullptr)
+        if(!root)
         return res;
         queue<TreeNode*>q;
         q.push(root);
@@ -25,66 +23,18 @@ public:
             vector<int>level;
             for(int i=0;i<n;i++)
             {
-                TreeNode* curr=q.front();
-                q.pop();
-                level.push_back(curr->val);
-                if(curr->left)
-                q.push(curr->left);
-                if(curr->right)
-                q.push(curr->right);
-
+                TreeNode*temp=q.front();
+                 q.pop();
+                level.push_back(temp->val);
+                 if(temp->left)
+                 q.push(temp->left);
+                 if(temp->right)
+                 q.push(temp->right);
 
             }
-            res.insert(res.begin(),level);
-
+          res.insert(res.begin(),level);
         }
+        return res;
         
-        return res;
-    }
-};
-
-*/
-
-
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    vector<vector<int>> levelOrderBottom(TreeNode* root) {
-        vector<vector<int>>res;
-        if(root==nullptr)
-        return res;
-        queue<TreeNode*>q;
-        q.push(root);
-        while(!q.empty())
-        {
-            int n=q.size();
-            vector<int>level;
-            for(int i=0;i<n;i++)
-            {
-                TreeNode* curr=q.front();
-                q.pop();
-                level.push_back(curr->val);
-                if(curr->left)
-                q.push(curr->left);
-                if(curr->right)
-                q.push(curr->right);
-
-
-            }
-            res.insert(res.end(),level);
-
-        }
-        reverse(res.begin(),res.end());
-        return res;
     }
 };
