76fdaa736c818bd025d56edbc7671d7cf144e725
DarshanDS004 <1si23is020@sit.ac.in>
2025-09-24 20:55:28 +0530

    Time: 0 ms (100%), Space: 11.7 MB (46.79%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,15 +1,27 @@
+
+
+
+        
+   
+
+
+
+#include <vector>
+
 class Solution {
 public:
-    int removeElement(vector<int>& nums, int val) {
-        int n=nums.size();
-        int k=0;
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]!=val)
-            {
-               nums[k++]=nums[i];
-            }
-        }
-        return k;
-    }
-};
+    int removeElement(std::vector<int>& nums, int val) {
+            int write_pointer = 0;
+                    
+                            // Iterate through the array and place non-val elements at the beginning
+                                    for (int i = 0; i < nums.size(); ++i) {
+                                                if (nums[i] != val) {
+                                                                nums[write_pointer] = nums[i];
+                                                                                write_pointer++;  // Move the write_pointer to the next position
+                                                                                            }
+                                                                                                    }
+                                                                                                            
+                                                                                                                    // The number of elements that are not equal to val
+                                                                                                                            return write_pointer;
+                                                                                                                                }
+                                                                                                                                };
