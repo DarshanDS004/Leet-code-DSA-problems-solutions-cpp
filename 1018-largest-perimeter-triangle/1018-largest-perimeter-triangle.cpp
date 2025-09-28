e3a6bc8a20745eacc41568242a8fc0dc7596ed39
DarshanDS004 <1si23is020@sit.ac.in>
2025-09-28 15:30:24 +0530

    Time: 3 ms (76.43%), Space: 25.8 MB (14.34%) - LeetHub

--- 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.cpp
+++ 1018-largest-perimeter-triangle/1018-largest-perimeter-triangle.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     int largestPerimeter(vector<int>& nums) {
@@ -7,6 +8,20 @@ public:
         {
             if(nums[i+1]+nums[i+2]>nums[i])
             return nums[i]+nums[i+1]+nums[i+2];
+        }
+        return 0;
+    }
+};
+*/
+class Solution {
+public:
+    int largestPerimeter(vector<int>& nums) {
+        sort(nums.begin(), nums.end(), greater<int>());
+
+        for (int i = 0; i < nums.size() - 2; ++i) {
+            if (nums[i + 1] + nums[i + 2] > nums[i]) {
+                return nums[i] + nums[i + 1] + nums[i + 2];
+            }
         }
         return 0;
     }
