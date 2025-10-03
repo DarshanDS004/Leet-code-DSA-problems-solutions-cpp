312b2eb7831e103e3b349efdc8041cde0d7e0083
DarshanDS004 <1si23is020@sit.ac.in>
2025-10-03 13:41:58 +0530

    Time: 19 ms (0.27%), Space: 47.6 MB (99.78%) - LeetHub

--- 0784-insert-into-a-binary-search-tree/0784-insert-into-a-binary-search-tree.cpp
+++ 0784-insert-into-a-binary-search-tree/0784-insert-into-a-binary-search-tree.cpp
@@ -11,25 +11,32 @@
  */
 class Solution {
 public:
-  TreeNode* insert(TreeNode*root,int val)
-  {
-    if(root==nullptr)
+    void inorder(TreeNode*root,vector<int>&nums)
     {
-        TreeNode* curr=new TreeNode(val);
+        if(root==nullptr)
+        return;
+        inorder(root->left,nums);
+        nums.push_back(root->val);
+        inorder(root->right,nums);
+    }
+
+    TreeNode*construct(vector<int>&nums,int left,int right)
+    {
+        if(left>right)
+        return nullptr;
+        int mid=(left+right)/2;
+        TreeNode*curr=new TreeNode(nums[mid]);
+        curr->left=  construct(nums,left,mid-1);
+        curr->right=construct(nums,mid+1,right);
         return curr;
+
     }
-    if(root->val>val)
-    {
-        root->left= insert(root->left, val);
-    }
-    else
-    {
-        root->right= insert(root->right, val);
-    }
-    return root;
-  }
     TreeNode* insertIntoBST(TreeNode* root, int val) {
-        return insert(root,val);
+        vector<int>res;
+        inorder(root,res);
+        res.push_back(val);
+        sort(res.begin(),res.end());
+        return construct(res,0,res.size()-1);
 
         
     }
