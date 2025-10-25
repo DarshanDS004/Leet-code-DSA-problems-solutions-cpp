f5e2822e7a829a7dc150e727cb9a1f773dbe3a1e
DarshanDS004 <1si23is020@sit.ac.in>
2025-10-25 22:45:14 +0530

    Time: 0 ms (100%), Space: 7.8 MB (86.15%) - LeetHub

--- 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.cpp
+++ 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.cpp
@@ -3,18 +3,18 @@ public:
     int totalMoney(int n) {
         int w=n/7;
         int d=n%7;
-        int val;
         int total=0;
-        
-        for(int i=0;i<w;i++){
+        int val;
+        for(int i=0;i<w;i++)
+        {
              val=i+1;
-            for(int i=0;i<7;i++)
+            for(int j=0;j<7;j++)
             {
-               total+=val;
-               val++;
+                total+=val;
+                val++;
             }
         }
-      val=w+1;
+        val=w+1;
         for(int i=0;i<d;i++)
         {
             total+=val;
