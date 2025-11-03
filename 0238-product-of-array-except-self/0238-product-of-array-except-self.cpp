0ab6a1e65ca534bd2eca38ba87173e06792f0c2f
DarshanDS004 <1si23is020@sit.ac.in>
2025-11-03 23:33:58 +0530

    Time: 4 ms (21.83%), Space: 42.1 MB (32.28%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -44,6 +44,7 @@ int backward(int p,int size,vector<int>&nums)
 };
 */
 
+/*
 class Solution {
 public:
 vector<int> productExceptSelf(vector<int>& nums) {
@@ -67,4 +68,56 @@ vector<int> productExceptSelf(vector<int>& nums) {
          }
          return res;
    }
+};
+*/
+
+
+/*
+class Solution {
+public:
+vector<int> productExceptSelf(vector<int>& nums) {
+     int n = nums.size();
+        vector<int> res(n);
+
+        for (int i = 0; i < n; i++) {
+            int product = 1;
+            for (int j = 0; j < n; j++) {
+                if (i != j) product *= nums[j];
+            }
+            res[i] = product;
+        }
+
+        return res;
+    }
+};
+
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> prefix(n, 1), suffix(n, 1), res(n);
+
+        // Compute prefix products
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1] * nums[i - 1];
+        }
+
+        // Compute suffix products
+        for (int i = n - 2; i >= 0; i--) {
+            suffix[i] = suffix[i + 1] * nums[i + 1];
+        }
+
+        // Multiply prefix[i] * suffix[i]
+        for (int i = 0; i < n; i++) {
+            res[i] = prefix[i] * suffix[i];
+        }
+
+        return res;
+    }
 };
