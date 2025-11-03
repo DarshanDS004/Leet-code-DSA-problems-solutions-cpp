7d4b0e8d3d0335c86ea9144ee4bf85265f92bdc6
DarshanDS004 <1si23is020@sit.ac.in>
2025-11-03 23:54:55 +0530

    Time: 63 ms (5.03%), Space: 127.8 MB (20.89%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -8,158 +8,39 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-/*
 class Solution {
 public:
-    bool isPalindrome(ListNode* head) {
-        string  res;
-        ListNode*temp=head;
-        while(temp!=nullptr)
-        {
-            res+=to_string(temp->val);
-            temp=temp->next;
-
-        }
-      int  n=res.size();
-      int low=0,high=n-1;
-      while(low<=high)
-      {
-        if(res[low]!=res[high])
-        return false;
-        low++;
-        high--;
-
-      }
-      return true;
-
-        
+string convert_to_str(ListNode* head)
+{
+    string res="";
+    ListNode* curr=head;
+    while(curr!=nullptr)
+    {
+        res +=to_string(curr->val);
+        curr=curr->next;
     }
-};
-*/
-/*
-class Solution {
-public:
-    bool isPalindrome(ListNode* head) {
-        vector<int>res;
-        ListNode*temp=head;
-        while(temp!=nullptr)
+    return res;
+
+}
+
+bool is_palindrome(string str)
+{
+    int n=str.size();
+    int low=0,high=n-1;
+    while(low<=high)
+    {
+        if(str[low]!=str[high])
         {
-            res.push_back(temp->val);
-            temp=temp->next;
-
-        }
-      int  n=res.size();
-      int low=0,high=n-1;
-      while(low<=high)
-      {
-        if(res[low]!=res[high])
-        return false;
-        low++;
-        high--;
-
-      }
-      return true;
-
-        
-    }
-};
-*/
-/*
-class Solution {
-public:
-    bool isPalindrome(ListNode* head) {
-        if(head->next==nullptr)
-        return true;
-        vector<int>res;
-        ListNode*slow=head;
-         ListNode*first=head;
-          ListNode*fast=head;
-          while(fast&&fast->next)
-          {
-            slow=slow->next;
-            fast=fast->next->next;
-          }
-          ListNode* second=slow;
-          ListNode*prev=NULL,*curr=second,*next;
-          while(curr)
-          {
-            next=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=next;
-          }
-          ListNode *rev=prev;
-          while(rev&&head)
-          {
-            if(rev->val!=first->val)
             return false;
-            rev=rev->next;
-            first=first->next;
-          }
-          return true;
+        }
+        low++;
+        high--;
+    }
+    return true;
+}
+    bool isPalindrome(ListNode* head) {
+        string res=convert_to_str(head);
+        return is_palindrome(res);
+        
     }
 };
-*/
-
-class Solution {
-public:
-
-    ListNode* reverse(ListNode* head)
-    {
-        if(head == nullptr || head->next == nullptr)
-            return head;
-        ListNode* prev = nullptr;
-        ListNode* next = nullptr;
-        ListNode* curr = head;
-        while(curr != nullptr)
-        {
-            next = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = next;
-        }
-        return prev;
-    }
-
-    ListNode* middle(ListNode* head)
-    {
-        if(head == nullptr || head->next == nullptr)
-            return head;
-        ListNode* slow = head;
-        ListNode* fast = head;
-        while(fast->next != nullptr && fast->next->next != nullptr)
-        {
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-        return slow;
-    }
-
-    bool isPalindrome(ListNode *head)
-    {
-        if(head == nullptr || head->next == nullptr)
-            return true;
-
-        ListNode* mid = middle(head);
-        ListNode* secondHalf = mid->next;
-        mid->next = nullptr;          // Break the list
-
-        ListNode* revmid = reverse(secondHalf);
-
-        ListNode* firstHalf = head;
-
-        while(revmid != nullptr)
-        {
-            if(firstHalf->val != revmid->val)
-                return false;
-            firstHalf = firstHalf->next;
-            revmid = revmid->next;
-        }
-
-        return true;
-    }
-};
-
-
-
-      
