f1201dba3f346ee3d61ee8e479054c40ffe1ecb6
DarshanDS004 <1si23is020@sit.ac.in>
2025-11-04 00:30:08 +0530

    Time: 6 ms (33.95%), Space: 121.7 MB (54.05%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -8,6 +8,8 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+ /*
 class Solution {
 public:
 string convert_to_str(ListNode* head)
@@ -44,3 +46,74 @@ bool is_palindrome(string str)
         
     }
 };
+
+
+*/
+
+class Solution {
+public:
+ListNode* find_middle(ListNode* head)
+{
+    ListNode*slow=head;
+    ListNode*fast=head;
+    while(fast && fast->next)
+    {
+        slow=slow->next;
+        fast=fast->next->next;
+
+    }
+    return slow;
+}
+
+ListNode *reverse_list(ListNode*head)
+{
+    ListNode*prev=NULL;
+    ListNode*curr=head;
+    ListNode*next=NULL;
+    while(curr)
+    {
+        next=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=next;
+        
+    }
+    return prev;
+}
+
+  bool is_same(ListNode* mid ,ListNode* sec_mid)
+  {
+    while(sec_mid)
+    {
+        if(sec_mid->val!=mid->val)
+        {
+            return false;
+        }
+        sec_mid=sec_mid->next;
+        mid=mid->next;
+
+    }
+    return true;
+  }
+
+
+
+ bool isPalindrome(ListNode* head) {
+    if(! head ||! head->next)
+    {
+        return true;
+    }
+      ListNode*middle=find_middle( head);
+      ListNode*sec_half=reverse_list(middle);
+      bool res=is_same(head,sec_half);
+      reverse_list(sec_half);
+      return res;
+
+
+   
+
+
+  }
+};
+
+
