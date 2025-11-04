c0c07141f03ab94f406c77314a92f6c6afecc2e4
DarshanDS004 <1si23is020@sit.ac.in>
2025-11-04 19:54:07 +0530

    Time: 8 ms (14.97%), Space: 14.4 MB (15.11%) - LeetHub

--- 1044-find-common-characters/1044-find-common-characters.cpp
+++ 1044-find-common-characters/1044-find-common-characters.cpp
@@ -1,3 +1,5 @@
+/*
+
 class Solution {
 public:
     vector<string> commonChars(vector<string>& words) {
@@ -26,5 +28,56 @@ public:
             }
           }
           return res;
+    }
+};
+
+
+*/
+
+
+
+class Solution {
+public:
+    vector<string> commonChars(vector<string>& words) {
+      unordered_map<char,int>mp;
+    
+      for(char st:words[0]){
+        mp[st]++;
+      }
+       
+       for(int i=1;i<words.size();i++)
+       {
+          unordered_map<char,int>frq;
+          for(char c:words[i])
+          {
+              frq[c]++;
+          }
+
+          for(auto it=mp.begin();it!=mp.end();)
+          {
+                if(frq.find(it->first)!=frq.end())
+                {
+                    it->second=min(it->second,frq[it->first]);
+                    it++;
+                }
+                else{
+                  it= mp.erase(it);
+                }
+           }
+
+
+       }
+
+       vector<string>res;
+       
+       for(auto p:mp)
+       {
+        for(int i=0;i<p.second;i++)
+        {
+            res.push_back(string(1,p.first));
+        }
+       }
+       return res;
+        
     }
 };
