fa960429be6babedb7fb4830588b3265b0cbdc44
DarshanDS004 <1si23is020@sit.ac.in>
2025-11-05 18:22:13 +0530

    Time: 15 ms (7.16%), Space: 70.4 MB (5.04%) - LeetHub

--- 2737-row-with-maximum-ones/2737-row-with-maximum-ones.cpp
+++ 2737-row-with-maximum-ones/2737-row-with-maximum-ones.cpp
@@ -1,31 +1,63 @@
+/*
 class Solution {
 public:
     vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
-        vector<int>res(2);
-        int n=mat.size();
-        int m=mat[0].size();
-        int count;
-        int max_count=-1;
-        int ans;
-        int i;
+        int n = mat.size();
+        int m = mat[0].size();
+        int max_count = -1;
+        int ans = -1;
 
-
-        for(i=0;i<n;i++)
-        {
-             count=0;
-           for(int j=0;j<m;j++)
-           {
-              if(mat[i][j]==1)
-              {
-                count++;
-              }
-           }
-           if(count>max_count)
-           {
-            max_count=count;
-            ans=i;
-         }
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+            for (int j = 0; j < m; j++) {
+                if (mat[i][j] == 1)
+                    count++;
+            }
+            if (count > max_count) {
+                max_count = count;
+                ans = i;
+            }
         }
-        return {ans,max_count};
+
+        return {ans, max_count};
+    }
+};
+*/
+
+class Solution {
+public:
+    int lower_bound(vector<int>& arr, int target) {
+        int n = arr.size();
+        int low = 0, high = n - 1;
+        int ans = n;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] >= target) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
+        int m = mat[0].size();
+        int max_count = 0;
+        int index = 0;
+        
+        for (int i = 0; i < mat.size(); i++) {
+            vector<int> row = mat[i];
+            sort(row.begin(), row.end());         
+            int first_one = lower_bound(row, 1);
+            int count = m - first_one;
+            if (count > max_count || (count == max_count && i < index)) {
+                max_count = count;
+                index = i;
+            }
+        }
+
+        return {index, max_count};
     }
 };
