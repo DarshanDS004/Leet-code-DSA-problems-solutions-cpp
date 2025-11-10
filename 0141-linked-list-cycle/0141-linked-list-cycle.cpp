34956323ce678b400eeb37051d9e0ced768a7a37
DarshanDS004 <1si23is020@sit.ac.in>
2025-11-10 06:40:32 +0530

    Time: 8 ms (80.2%), Space: 12 MB (21.69%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -33,6 +33,7 @@ public:
 };
 */
 
+/*
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
@@ -50,4 +51,27 @@ public:
     }
         return false;
   }
+};
+
+*/
+
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+    if (!head || !head->next) 
+    return false;
+    
+    ListNode*slow=head;
+    ListNode*fast=head;
+    while(fast &&fast->next)
+    {
+        
+        slow=slow->next;
+        fast=fast->next->next;
+        if(slow==fast)
+        return true;
+    }
+    return false;
+    }
 };
