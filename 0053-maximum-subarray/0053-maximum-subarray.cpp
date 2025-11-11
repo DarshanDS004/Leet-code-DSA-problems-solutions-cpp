025e10efcd7b66249116d8982a9f48c34c936de9
DarshanDS004 <1si23is020@sit.ac.in>
2025-11-11 20:35:07 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,37 +1,19 @@
-/*
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         int n=nums.size();
-        int sum=0;
-        int max_sum=nums[0];
+        int sum=0; 
+        int max_sum=0;
         for(int i=0;i<n;i++)
         {
-               sum+=nums[i];
-               max_sum=max(max_sum,sum);
-               if(sum<0)
-               {
+              sum+=nums[i];
+              if(sum<0){
                 sum=0;
-               }
+              }
+              max_sum=max(max_sum,sum);
         }
         return max_sum;
+
         
-    }
-};
-*/
-class Solution {
-public:
-    int maxSubArray(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>dp(n);
-        dp[0]=nums[0];
-        int maxi=dp[0];
-        int sum=0;
-        for(int i=1;i<n;i++)
-        {
-            dp[i]=max(nums[i],nums[i]+dp[i-1]);
-            maxi=max(maxi,dp[i]);
-        }
-        return maxi;
     }
 };
