9e4673e27195bef519200af1718951a6ecf22560
DarshanDS004 <1si23is020@sit.ac.in>
2025-11-14 08:21:59 +0530

    Time: 48 ms (27.85%), Space: 84.9 MB (7.73%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -1,84 +1,31 @@
-//BRUTE FORCE TLE 
-/*
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
         int n=fruits.size();
-         int maxlen=0;
-        for(int i=0;i<n;i++)
-        {
-           
-            set<int>st;
-            for(int j=i;j<n;j++)
-            {
-                st.insert(fruits[j]);
-                if(st.size()<=2)
-                {
-                    maxlen=max(maxlen,j-i+1);
-                }
-                else
-                break;
-
-
-            }
-        }
-        return maxlen;
-        
-        
-    }
-};
-
-*/
-
-/*
-
-class Solution {
-public:
-    int totalFruit(vector<int>& fruits) {
-        int n=fruits.size();
-        int maxlen=0;
-        int left=0;
+        int left=0,right=0,max_len=0;
         unordered_map<int,int>mp;
-        for(int right=0;right<n;right++)
+        while(right<n)
         {
             mp[fruits[right]]++;
             while(mp.size()>2)
             {
                 mp[fruits[left]]--;
                 if(mp[fruits[left]]==0)
+                {
                 mp.erase(fruits[left]);
+                }
                 left++;
-                
             }
-            maxlen=max(maxlen,right-left+1);
-        }
-        return maxlen;
-    }
-};
 
-*/
-
-class Solution {
-public:
-    int totalFruit(vector<int>& fruits) {
-        int n=fruits.size();
-        int maxlen=0;
-        int left=0;
-        unordered_map<int,int>mp;
-        for(int right=0;right<n;right++)
-        {
-            mp[fruits[right]]++;
-            if(mp.size()>2)
-            {
-                mp[fruits[left]]--;
-                if(mp[fruits[left]]==0)
-                mp.erase(fruits[left]);
-                left++;
-                
-            }
             if(mp.size()<=2)
-            maxlen=max(maxlen,right-left+1);
+            {
+                max_len=max(max_len,right-left+1);
+                right++;
+            }
         }
-        return maxlen;
+        return max_len;
+
+        
+        
     }
 };
