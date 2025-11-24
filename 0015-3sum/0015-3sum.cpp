02872c1717a93fd888a18f4c18e92508ea5521d9
DarshanDS004 <1si23is020@sit.ac.in>
2025-11-24 20:12:01 +0530

    Time: 2155 ms (5%), Space: 471.7 MB (5%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,102 +1,31 @@
-/*
-
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-       
-         set<vector<int>> st;
-        int n=nums.size();
-        for(int i=0;i<n;i++)
-        {
-            for(int j=i+1;j<n;j++)
-            {
-                for(int k=j+1;k<n;k++)
-                {
-                    if(nums[i]+nums[j]+nums[k]==0)
-                    {
-                        vector<int>temp={nums[i],nums[j],nums[k]};
-                        sort(temp.begin(),temp.end());
-                        st.insert(temp);
-                        
-                    }
-
-                }
-            }
-        }
-         vector<vector<int>>res(st.begin(),st.end());
-        return res;
+   vector<vector<int>> threeSum(vector<int>& nums) {
+    //vector<vector<int>> threeSumUsingMap(vector<int>& nums) {
+    vector<vector<int>> result;
+    int n = nums.size();
+    
+    if (n < 3) return result;
+    
+    sort(nums.begin(), nums.end());  // Sort the array
+    
+    for (int i = 0; i < n - 2; i++) {
+        // Skip duplicates for the first element
+        if (i > 0 && nums[i] == nums[i - 1]) continue;
         
-    }
-};
-*/
-
-
-/*
-class Solution {
-public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-       
-         set<vector<int>> st;
-        int n=nums.size();
-        for(int i=0;i<n;i++)
-        {
-                set<int>mp;
-            for(int j=i+1;j<n;j++)
-            {
-              
-                int third=-(nums[i]+nums[j]);
-                if(mp.find(third)!=mp.end())
-                {
-                    vector<int>temp={nums[i],nums[j],third};
-                    sort(temp.begin(),temp.end());
-                    st.insert(temp);
-                }
-              mp.insert(nums[j]);
-
+        unordered_set<int> seen;
+        for (int j = i + 1; j < n; j++) {
+            int complement = -(nums[i] + nums[j]);
+            if (seen.find(complement) != seen.end()) {
+                result.push_back({nums[i], nums[j], complement});
+                
+                // Skip duplicates for the second element
+                while (j + 1 < n && nums[j] == nums[j + 1]) j++;
             }
+            seen.insert(nums[j]);
         }
-         vector<vector<int>>res(st.begin(),st.end());
-         return res;
     }
-};
-*/
-
-class Solution {
-public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
-        vector<vector<int>> res;
-
-        for (int i = 0; i < n; i++) {
-
-
-            if (i > 0 && nums[i] == nums[i - 1])
-                continue;
-
-            int left = i + 1, right = n - 1;
-
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if (sum < 0) left++;
-                else if (sum > 0) right--;
-                else {
-                    res.push_back({nums[i], nums[left], nums[right]});
-
-                    left++;
-                    right--;
-
-                    
-                    while (left < right && nums[left] == nums[left - 1])
-                        left++;
-
-                    while (left < right && nums[right] == nums[right + 1])
-                        right--;
-                }
-            }
-        }
-
-        return res;
-    }
+    
+    return result;
+}
 };
