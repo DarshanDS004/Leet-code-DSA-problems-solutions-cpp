5fdbd9012d9fcb6da5ce02664e75efecb7adadc4
DarshanDS004 <1si23is020@sit.ac.in>
2026-01-23 22:53:59 +0530

    Time: 0 ms (100%), Space: 11.6 MB (81.96%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -3,16 +3,17 @@
 class Solution {
 public:
     int removeElement(std::vector<int>& nums, int val) {
-            int write_pointer = 0;
-                    
-                            // Iterate through the array and place non-val elements at the beginning
-                                    for (int i = 0; i < nums.size(); ++i) {
-                                                if (nums[i] != val) {
-                                                                nums[write_pointer] = nums[i];
-                                                                                write_pointer++;  // Move the write_pointer to the next position
-                                                                                            }
-                                                                                                    }
-                                                                                                            
-                                                                                                                    // The number of elements that are not equal to val
-                                                                                                                            return write_pointer;
-                                            
+        int write_pointer = 0;
+        
+        // Iterate through the array and place non-val elements at the beginning
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] != val) {
+                nums[write_pointer] = nums[i];
+                write_pointer++;  // Move the write_pointer to the next position
+            }
+        }
+        
+        // The number of elements that are not equal to val
+        return write_pointer;
+    }
+};  
