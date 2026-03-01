5c9885957ad97b76631e0382b05cb8a3e39ef9fb
DarshanDS004 <1si23is020@sit.ac.in>
2026-03-02 00:43:37 +0530

    Time: 3 ms (14.43%), Space: 9.2 MB (21.63%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,77 +1,42 @@
 class Solution {
 public:
     int myAtoi(string s) {
+        int n=s.length();
         int i=0;
-        while(i<s.length()&&s[i]==' ')
-        {
-           
-            i++;
-        }
-        if(i>=s.length())
-        return 0;
         int sign=1;
         long ans=0;
+        while(i<n && s[i]==' ')
+        i++;
         if(s[i]=='-')
         {
         sign=-1;
         i++;
         }
-        else if(s[i]=='+')
+          else if(s[i]=='+')
         {
-            i++;
+       
+        i++;
         }
-
-        int min=INT_MIN,max=INT_MAX; 
-        while(i<s.length()&&isdigit(s[i]))
+        while(i<n)
         {
-   
-              ans=ans*10+(s[i]-'0');
-              if(sign==-1&&(-1*ans)<min)
-            return min;
-             if(sign==1&&(ans*1)>max)
-             return max;
-             i++;
+            if(s[i]>='0'&&s[i]<='9')
+            {
+                ans=ans*10+(s[i]-'0');
+                if(ans>INT_MAX&&sign==-1)
+                return INT_MIN;
+                else if(ans>INT_MAX&&sign==1)
+                return INT_MAX;
+                i++;
+            }
+            else
+            {
+               return ans*sign;
+            }
 
         }
-        return (int)(sign*ans);
+        return (ans*sign);
+
+
         
     }
 };
-
-
-// class Solution {
-// public:
-//     int myAtoi(string s) {
-//         int i = 0;
-//         // 1. Skip leading whitespaces
-//         while (i < s.length() && s[i] == ' ') {
-//             i++;
-//         }
-
-//         // If string is all spaces
-//         if (i >= s.length()) return 0;
-
-//         // 2. Handle sign
-//         int sign = 1;
-//         if (s[i] == '-') {
-//             sign = -1;
-//             i++;
-//         } else if (s[i] == '+') {
-//             i++;
-//         }
-
-//         // 3. Convert digits to integer
-//         long ans = 0;
-//         while (i < s.length() && isdigit(s[i])) {
-//             ans = ans * 10 + (s[i] - '0');
-
-//             // 4. Clamp to 32-bit signed int
-//             if (sign == -1 && -1 * ans < INT_MIN) return INT_MIN;
-//             if (sign == 1 && ans > INT_MAX) return INT_MAX;
-
-//             i++;
-//         }
-
-//         return static_cast<int>(sign * ans);
-//     }
-// };
