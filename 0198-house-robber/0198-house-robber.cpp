9abb0c306fd4101157f9425741c9008219dcfe0b
DarshanDS004 <1si23is020@sit.ac.in>
2026-04-08 22:22:58 +0530

    Time: 0 ms (100%), Space: 10.8 MB (21.33%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,48 +1,45 @@
 /*
 class Solution {
 public:
-   int helper(vector<int>& nums,int i,int n )
-   {
-    if(i>=n)
-    {
-        return 0;
-    }
-
-    int skip=helper(nums,i+1,n);
-    int not_skip=nums[i]+helper(nums,i+2,n);
-
-    return max(skip,not_skip);
-
- }
+int helper(int index,vector<int>&arr){
+    int n=arr.size();
+    if(index>=n)
+    return 0;
+    int take=arr[index]+helper(index+2,arr);
+    int not_take=helper(index+1,arr);
+    return max(take ,not_take);
+}
+ 
     int rob(vector<int>& nums) {
-        int n=nums.size();
-        return helper(nums,0,n);
+        return helper(0,nums);
         
     }
 };
+
 */
+
 
 class Solution {
 public:
-int helper(int idx,vector<int>&nums,vector<int>&dp)
-{
-    if(idx==0)
-    return nums[idx];
-    if(idx<0)
+int helper(int index,vector<int>&arr,vector<int>&dp){
+    int n=arr.size();
+    if(index>=n)
     return 0;
-    if(dp[idx]!=-1)
-    return dp[idx];
+    if(dp[index]!=-1)
+    return dp[index];
 
-    int pick=nums[idx]+helper(idx-2,nums,dp);
-    int not_pick=0+helper(idx-1,nums,dp);
-    return dp[idx]=max(pick,not_pick);
+    int take=arr[index]+helper(index+2,arr,dp);
 
-    
+    int not_take=helper(index+1,arr,dp);
+
+    return dp[index]=max(take, not_take);
 }
- int rob(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>dp(n,-1);
-        return helper(n-1,nums,dp);
 
-  }
+
+ int rob(vector<int>& nums) {
+   vector<int>dp(nums.size(),-1);
+   return helper(0,nums,dp);
+ }
 };
+
+
