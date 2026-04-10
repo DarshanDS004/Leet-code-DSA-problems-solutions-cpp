ba2f9e96b4df7f0360be72b492cd45937579d951
DarshanDS004 <1si23is020@sit.ac.in>
2026-04-10 22:56:53 +0530

    Time: 423 ms (72.05%), Space: 0B (100%) - LeetHub

---
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select id 
+from (select id ,recordDate,temperature ,
+                    lag(temperature) over(order by recordDate) as prev_temp,
+                    lag(recordDate) over(order by recordDate) as prev_date
+                    from Weather
+                    )  t
+                    where temperature>prev_temp
+                    and  datediff(recordDate,prev_date)=1;
