a47f86d9ad23cc12a6b0a6436595fb0331728014
DarshanDS004 <1si23is020@sit.ac.in>
2026-05-28 07:11:25 +0530

    Time: 0 ms (100%), Space: 8.3 MB (14.21%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.cpp
+++ 0709-to-lower-case/0709-to-lower-case.cpp
@@ -3,7 +3,11 @@ public:
     string toLowerCase(string s) {
          string res;
         for(int i=0;i<s.size();i++){
-            res+=tolower(s[i]);
+            if(s[i]>='A' && s[i]<='Z')
+            res+=s[i]+32;
+            else{
+                res+=s[i];
+            }
            
         }
          return res;
