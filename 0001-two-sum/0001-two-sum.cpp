68373e19549d5aba262dfc57789b9dcafa68cb2c
DarshanDS004 <1si23is020@sit.ac.in>
2026-05-30 07:06:55 +0530

    Time: 34 ms (34.91%), Space: 13.9 MB (94.38%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
@@ -11,6 +12,24 @@ public:
             }
             mp[nums[i]]=i;
 
+        }
+        return {};
+    }
+};
+
+*/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+
+            for(int j=i+1;j<n;j++){
+                if(nums[i]+nums[j]==target){
+                    return {i,j};
+                }
+            }
         }
         return {};
     }
