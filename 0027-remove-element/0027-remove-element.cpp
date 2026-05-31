269bb3bcc59346d1c20405d5d56416bba4cc7fa0
DarshanDS004 <1si23is020@sit.ac.in>
2026-05-31 06:08:59 +0530

    Time: 0 ms (100%), Space: 11.7 MB (83.38%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,19 +1,15 @@
-#include <vector>
-
 class Solution {
 public:
-    int removeElement(std::vector<int>& nums, int val) {
-        int write_pointer = 0;
-        
-        // Iterate through the array and place non-val elements at the beginning
-        for (int i = 0; i < nums.size(); ++i) {
-            if (nums[i] != val) {
-                nums[write_pointer] = nums[i];
-                write_pointer++;  // Move the write_pointer to the next position
+    int removeElement(vector<int>& nums, int val) {
+        int n=nums.size();
+        int j=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=val){
+                nums[j++]=nums[i];
+
             }
         }
+        return j;
         
-        // The number of elements that are not equal to val
-        return write_pointer;
     }
-};  
+};
