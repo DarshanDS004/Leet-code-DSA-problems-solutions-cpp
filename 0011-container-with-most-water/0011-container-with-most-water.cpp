93fc8c86c969bf9737bd64b664182230e63ddc8c
DarshanDS004 <1si23is020@sit.ac.in>
2026-06-01 06:23:38 +0530

    Time: 62 ms (7.13%), Space: 75.7 MB (10.38%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,75 +1,48 @@
-
 /*
+
 class Solution {
 public:
     int maxArea(vector<int>& height) {
         int n=height.size();
         int max_area=0;
+
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
-
+                
                 int width=j-i;
                 int min_height=min(height[i],height[j]);
                 max_area=max(max_area,width*min_height);
-                
             }
         }
         return max_area;
+        
     }
 };
 
 */
 
-/*
 class Solution {
 public:
     int maxArea(vector<int>& height) {
         int n=height.size();
         int max_area=0;
-        vector<pair<int,int>>a;
+        vector<pair<int,int>>arr;
         for(int i=0;i<n;i++){
-            a.push_back({height[i],i});
+            arr.push_back({height[i],i});
         }
-     sort(a.begin(),a.end(),greater<pair<int,int>>());
+        sort(arr.begin(),arr.end(),greater<pair<int,int>>());
 
+        int min_index=arr[0].second;
+        int max_index=arr[0].second;
 
-     int min_index=a[0].second;
-     int max_index=a[0].second;
-     
-     for(int i=0;i<a.size();i++){
-        int max_width=max(abs(a[i].second-max_index),abs(a[i].second-min_index));
-        int curr_area= 1LL*a[i].first*max_width;
-         max_area=max(max_area,curr_area);
-
-         min_index=min(min_index,a[i].second);
-         max_index=max(max_index,a[i].second);
-     }
-     return max_area;
-    }
-};
-
-*/
-
-class Solution {
-public:
-    int maxArea(vector<int>& height) {
-        int n=height.size();
-        int max_area=0;
-        int left=0,right=n-1;
-        while(left<right){
-          
-         int width= right-left;
-         int min_height=min(height[left],height[right]);
-          max_area=max(max_area,width*min_height);
-
-          if(height[left]>height[right]){
-            right--;
-          }
-          else{
-            left++;
-          }
+        for(int i=0;i<n;i++){
+            
+            int width=max(abs(arr[i].second-min_index),abs(arr[i].second-max_index));
+            int curr_area=width*arr[i].first;
+            max_area=max(max_area,curr_area);
+            min_index=min(min_index,arr[i].second);
+            max_index=max(max_index,arr[i].second);
         }
         return max_area;
     }
 };
-
