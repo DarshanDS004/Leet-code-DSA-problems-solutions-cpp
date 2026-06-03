1949352aba83afa66d59ce7562d1f3d8b3f1c044
DarshanDS004 <1si23is020@sit.ac.in>
2026-06-03 23:57:09 +0530

    Time: 9 ms (17.37%), Space: 23.4 MB (98.06%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
@@ -15,5 +16,29 @@ public:
          }
          return res;
         
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n=intervals.size();
+        sort(intervals.begin(),intervals.end());
+        int idx=0;
+        for(int i=1;i<n;i++){
+            if(intervals[i][0]<=intervals[idx][1]){
+                intervals[idx][1]=max(intervals[i][1],intervals[idx][1]);
+                
+            }
+            else{
+                idx++;
+                intervals[idx]=intervals[i];
+                
+            }
+        }
+         intervals.resize(idx + 1);
+
+        return intervals;
     }
 };
