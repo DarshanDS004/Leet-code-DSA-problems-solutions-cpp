c34deff81a0d4d8b7e4be78689599f03d7b1a1a9
DarshanDS004 <1si23is020@sit.ac.in>
2026-06-03 15:17:17 +0530

    Time: 0 ms (100%), Space: 10.1 MB (91.54%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
@@ -12,5 +13,24 @@ public:
 
         matrix=res;
         
+    }
+};
+*/
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
     }
 };
