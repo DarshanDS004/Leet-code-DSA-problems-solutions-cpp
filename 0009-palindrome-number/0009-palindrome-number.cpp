5c5965dea2c4cfc70351c5f5106f3905cac63e6c
DarshanDS004 <1si23is020@sit.ac.in>
2026-06-08 23:38:39 +0530

    Time: 3 ms (45.43%), Space: 8.6 MB (37.53%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,28 +1,15 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        long rev=0, ori=x;
-       
-
-        while(x>0)
-        {
-          int m=x%10;
-          rev=rev*10+m;
-          x/=10;
-
-          
-        }
-        if(rev==ori)
-        return true;
+        if(x<0)
         return false;
-        
-        
-        /*string str = to_string(x);
-           string st=str;
-          reverse(str.begin(), str.end());
-        return str==st;
-        */
-
+        int original=x;
+        long long num=0;
+        while(x){
+            num=num*10+x%10;
+            x/=10;
+        }
+        return original==num;
         
     }
 };
