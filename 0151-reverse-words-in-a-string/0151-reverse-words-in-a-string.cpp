90fa3f6ce7d3fe1c9b15f41b1323f177fa7f00ee
DarshanDS004 <1si23is020@sit.ac.in>
2026-06-08 17:51:16 +0530

    Time: 0 ms (100%), Space: 10.1 MB (69.45%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int n=s.size();
+        string res;
+        int i=n-1;
+        while(i>=0){
+
+        while(i>=0 && s[i]==' '){
+            i--;
+        }
+         if(i<0)
+         break;
+        
+
+        int j=i;
+        while(j>=0 && s[j]!=' '){
+            j--;
+        }
+
+        if(!res.empty()){
+            res+=' ';
+        }
+        res+=s.substr(j+1,i-j);
+
+        i=j-1;
+    }
+        
+      return res;
+    }
+};
