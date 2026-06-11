3aa4525cccc982bce49f758ce74647940f7ff0aa
DarshanDS004 <1si23is020@sit.ac.in>
2026-06-11 22:21:58 +0530

    Time: 62 ms (29.36%), Space: 23.8 MB (84.96%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -38,6 +38,7 @@ public:
 
 */
 
+/*
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
@@ -84,6 +85,25 @@ public:
           
     }
 
+};
+*/
 
-   
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+
+        if(headA==nullptr && headB==nullptr)
+        return nullptr;
+        
+        ListNode*temp1=headA;
+        ListNode*temp2=headB;
+        
+         while( temp1!=temp2){
+
+            temp1=(temp1==nullptr) ? headB:temp1->next;
+            temp2=(temp2==nullptr) ? headA:temp2->next;
+
+         }
+         return temp1;
+    }
 };
