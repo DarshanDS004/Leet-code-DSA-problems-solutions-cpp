748ad0e15e1de3671f84947bc0521884ae1fffa8
DarshanDS004 <1si23is020@sit.ac.in>
2026-06-11 18:46:35 +0530

    Time: 0 ms (100%), Space: 9.9 MB (90.13%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -13,12 +13,12 @@ public:
     ListNode* middleNode(ListNode* head) {
         ListNode* slow=head;
         ListNode*fast=head;
+
         while(fast && fast->next){
             slow=slow->next;
             fast=fast->next->next;
         }
         return slow;
-    
         
     }
 };
