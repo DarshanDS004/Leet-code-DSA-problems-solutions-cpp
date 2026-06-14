98d9320a5e4593b22de5fc703b0a2fcad80933ee
DarshanDS004 <1si23is020@sit.ac.in>
2026-06-14 17:32:30 +0530

    Time: 16 ms (7.33%), Space: 15.1 MB (5.67%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -6,72 +6,19 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-
-/*
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if (!head || !head->next)
-         return false;
-        vector<ListNode*>arr;
-        ListNode*curr=head;
-        int i=0;
-        while(curr)
-        {
-            for(ListNode* node:arr)
-            {
-                if(node==curr)
+        unordered_map<ListNode*,int>mp;
+        ListNode*temp=head;
+        while(temp){
+            if(mp.count(temp)){
                 return true;
             }
-            arr.push_back(curr);
-            curr=curr->next;
+            mp[temp]=temp->val;
+            temp=temp->next;
         }
         return false;
-      
         
-    }
-};
-*/
-
-/*
-class Solution {
-public:
-    bool hasCycle(ListNode *head) {
-    if (!head || !head->next) 
-    return false;
-    unordered_set<ListNode*>seen;
-    ListNode*curr=head;
-    while(curr)
-    {
-        if(seen.count(curr))
-        return true;
-
-        seen.insert(curr);
-        curr=curr->next;
-    }
-        return false;
-  }
-};
-
-*/
-
-
-class Solution {
-public:
-    bool hasCycle(ListNode *head) {
-    if (!head || !head->next) 
-    return false;
-    
-    ListNode*slow=head;
-    ListNode*fast=head;
-    while(fast &&fast->next)
-    {
-        
-        slow=slow->next;
-        fast=fast->next->next;
-        if(slow==fast)
-        return true;
-    }
-    return false;
     }
 };
