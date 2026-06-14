546459484253022a7d88c6cd96724ab109f125a9
DarshanDS004 <1si23is020@sit.ac.in>
2026-06-14 07:18:08 +0530

    Time: 0 ms (100%), Space: 12.9 MB (37.62%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,71 +1,25 @@
-/*
-class Solution {
-public:
-    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        int m=nums1.size();
-        int n=nums2.size();
-        vector<int>res;
-        for(int i=0;i<m;i++)
-        {
-            int ele=nums1[i];
-           int next_greater=-1;
-            for(int j=0;j<n;j++)
-            {
-                if(nums2[j]==ele)
-                {
-                    for(int k=j+1;k<n;k++)
-                    {
-                        if(nums2[k]>ele)
-                        {
-                            next_greater=nums2[k];
-                            break;
-                        }
-                    }
-                    break;
-                }
-            }
-            res.push_back(next_greater);
-        }
-        return res;
-        
-    }
-};
-
-*/
-
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         unordered_map<int,int>mp;
         stack<int>st;
-        for(int i=nums2.size()-1;i>=0;i--)
-        {
-            while(!st.empty()&&nums2[i]>=st.top())
-            {
-                st.pop();
+        for(int x:nums2){
+            while(!st.empty() && st.top()<x){
+                  mp[st.top()]=x;
+                  st.pop();
             }
-            
-            if(st.empty())
-            {
-                mp[nums2[i]]=-1;
-            }
-            else
-            {
-                mp[nums2[i]]=st.top();
-            }
-            st.push(nums2[i]);
+            st.push(x);
         }
+
+        while(!st.empty()){
+            mp[st.top()]=-1;
+            st.pop();
+        }
+
         vector<int>res;
-        for(int ele:nums1)
-        {
-            res.push_back(mp[ele]);
+        for(int y:nums1){
+            res.push_back(mp[y]);
         }
-        return res;
-
-
-
-  
+         return res;
     }
 };
-
-
