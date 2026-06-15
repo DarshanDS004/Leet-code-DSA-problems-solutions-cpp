eaf59913e56bf2897fcfd459ac9af3480f60b823
DarshanDS004 <1si23is020@sit.ac.in>
2026-06-15 10:43:30 +0530

    Time: 2 ms (13.97%), Space: 24 MB (7.08%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -1,57 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
+    void reorderList(ListNode* head) {
+      if(head==nullptr ||head->next==nullptr)
+      return;
 
-ListNode* middle(ListNode* head){
-    ListNode *slow=head;
-    ListNode *fast=head;
+      vector<ListNode*>arr;
+      ListNode*temp=head;
+      while(temp){
+        arr.push_back(temp);
+        temp=temp->next;
+      }
+        int n=arr.size();
+        int i=0;
+        int j=n-1;
+        ListNode*prev=nullptr;
+        while(i<j){
+          
+          arr[i]->next=arr[j];
+          i++;
 
-    while(fast->next && fast->next->next){
-        slow=slow->next;
-        fast=fast->next->next;
+          if(i>=j)
+          break;
+
+          arr[j]->next=arr[i];
+          j--;
+        }
+        arr[i]->next=nullptr;
+    
     }
-
-    return slow;
-}
-
-ListNode* reverse(ListNode *head)
-{
-    ListNode *prev=nullptr;
-    ListNode *curr=head;
-    ListNode *next=nullptr;
-
-    while(curr){
-        next=curr->next;
-        curr->next=prev;
-        prev=curr;
-        curr=next;
-    }
-
-    return prev;
-}
-
-void reorderList(ListNode* head) {
-
-    if(!head || !head->next)
-        return;
-
-    ListNode *first=head;
-
-    ListNode *second=middle(head);
-
-    ListNode *third=reverse(second->next);
-
-    second->next=NULL;
-
-    while(third){
-
-        ListNode* temp1=first->next;
-        ListNode* temp2=third->next;
-
-        first->next=third;
-        third->next=temp1;
-
-        first=temp1;
-        third=temp2;
-    }
-}
 };
