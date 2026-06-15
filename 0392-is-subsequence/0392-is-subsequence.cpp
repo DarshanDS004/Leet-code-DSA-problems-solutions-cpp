8051fe4b09c98d1219ebb79df85d7a5e9522f289
DarshanDS004 <1si23is020@sit.ac.in>
2026-06-15 22:24:00 +0530

    Time: 0 ms (100%), Space: 8.6 MB (66.5%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
@@ -22,3 +23,28 @@ public:
         return true;
     }
 };
+
+*/
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int n=s.size();
+        int m=t.size();
+         int i=0,j=0;
+        while(i< n && j<m){
+
+            if(s[i]==t[j]){
+                i++;
+            }
+            j++;
+        }
+    
+    if(i==n)
+    return true;
+    else
+    return false;
+     }
+};
+
+
