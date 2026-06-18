ea4a104c09b4c91901dc226995a7c9eca394ba07
DarshanDS004 <1si23is020@sit.ac.in>
2026-06-18 07:41:52 +0530

    Time: 65 ms (12.43%), Space: 34.4 MB (9.76%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,3 +1,5 @@
+
+/*
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
@@ -14,6 +16,44 @@ public:
         for(auto [key,val]:mp){
             res.push_back(val);
         }
+        return res;
+    }
+};
+
+*/
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string ,vector<string>>mp;
+        vector<vector<string>>res;
+
+        for(string s:strs){
+           string word=s;
+
+           vector<int>freq(26,0);
+
+           for(char c:word){
+            freq[c-'a']++;
+           }
+
+           string key;
+
+           for(int x:freq){
+            key+=to_string(x);
+            key+='#';
+           }
+           
+
+           mp[key].push_back(word);
+        }
+        
+
+           for(auto &it :mp){
+
+            res.push_back(it.second);
+           }
+
+        
         return res;
     }
 };
