5eb96593aac3614c6490b21096c87af127ad00ef
DarshanDS004 <1si23is020@sit.ac.in>
2026-06-19 20:57:05 +0530

    Time: 0 ms (100%), Space: 10.7 MB (94.12%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     int largestAltitude(vector<int>& gain) {
@@ -12,6 +13,26 @@ public:
                 max_gain=res[i];
             }
             
+        }
+        return max_gain;
+    }
+};
+
+*/
+
+
+
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int n=gain.size();
+        int max_gain=0;
+
+        int start=max_gain;
+        for(int i=0;i<n;i++){
+          start+=gain[i];
+          max_gain=max(max_gain, start) ;
+          
         }
         return max_gain;
     }
