c4658bd016811bbdcc117ea9f811eeaee1fe2914
DarshanDS004 <1si23is020@sit.ac.in>
2026-06-20 07:17:41 +0530

    Time: 23 ms (66.42%), Space: 24 MB (81.36%) - LeetHub

--- 3614-process-string-with-special-operations-ii/3614-process-string-with-special-operations-ii.cpp
+++ 3614-process-string-with-special-operations-ii/3614-process-string-with-special-operations-ii.cpp
@@ -1,94 +1,50 @@
-
-/*
 class Solution {
 public:
-
-    void reverse(string &s ){
-        int l=0,r=s.size()-1;
-
-        while(l<r){
-            swap(s[l++],s[r--]);
-        }
-    }
     char processStr(string s, long long k) {
         int n=s.size();
-        string res="";
-       for(int i=0;i<n;i++){
+        long long len=0;
+        long long max_len=0;
         
-        if(islower(s[i])){
-            res+=s[i];
-        }
-        else if(s[i]=='*' && !res.empty()){
-            res.pop_back();
-        }
-        else if(s[i]=='#'){
-            res+=res;
-        }
-        else{
-            reverse(res);
+        for(char c:s){
+            if(c=='*'){
+              len=max(0LL,len-1);
+            }
+            else if(c=='#'){
+                len*=2;
+            }
+            else if(c!='%'){
+                len++;
+            }
         }
 
-       }
 
-       if(k<res.size()){
-        return res[k];
-       }
-       else{
+
+        if(k>=len)
         return '.';
-       }
 
+        for(int i=n-1;i>=0;i--){
+            if(s[i]=='*'){
+                len++;
+                
+
+            }
+            else if(s[i]=='%'){
+                k=len-1-k;
+            }
+            else if(s[i]=='#'){
+                len/=2;
+
+                if(k>=len){
+                    k-=len;
+                }
+            }
+            else {
+                len--;
+                if(k==len)
+                return s[i];
+            }
+        }
+    return '.';
         
     }
 };
-
-*/
-class Solution {
-public:
-char processStr(string s, long long k) {
-     int n=s.size();
-      long long len=0;
-
-      for(char c :s){
-        
-        if(c=='*'){
-            len=max(0LL,len-1);
-        }
-        else if(c=='#'){
-            len*=2;
-        }
-        else if(c!='%'){
-            len++;
-        }
-
-      }
-
-      if(k>=len)
-      return '.';
-
-      for(int i=n-1;i>=0;i--){
-        char ch=s[i];
-       
-       if( ch=='*'){
-          len++;
-       }
-       else if(ch=='#'){
-        len/=2;
-        k%=len;
-       }
-
-       else if(ch=='%'){
-        k=len-1-k;
-       }
-       else{
-        len--;
-
-        if(len==k){
-            return ch;
-        }
-       }
-
- }
- return '.';
-}
-};
-       
