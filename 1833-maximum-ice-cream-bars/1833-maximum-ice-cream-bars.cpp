756ec842678d5a6438358de3c89fe931b52883e7
DarshanDS004 <1si23is020@sit.ac.in>
2026-06-21 13:58:18 +0530

    Time: 117 ms (5.38%), Space: 304.4 MB (5.01%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     int maxIceCream(vector<int>& costs, int coins) {
@@ -14,5 +15,31 @@ public:
         }
         return count;
         
+    }
+};
+*/
+
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        int n=costs.size();
+        vector<int>freq(1000001,0);
+
+        for(int i:costs){
+            freq[i]++;
+        }
+         
+         int count=0;
+        for(int i=1;i<=100000;i++){
+
+            int possible=min(freq[i],coins/i);
+            count+=possible;
+            coins-=possible*i;
+
+            if(coins<i)
+            break;
+
+        }
+        return count;
     }
 };
