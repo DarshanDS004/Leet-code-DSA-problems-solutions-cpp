e71fe45c3679d3a2234bfa035a7c171646c95772
DarshanDS004 <1si23is020@sit.ac.in>
2026-06-21 07:23:31 +0530

    Time: 3 ms (28.39%), Space: 95.9 MB (18.09%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,37 +1,43 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int m = nums1.size();
-        int n = nums2.size();
-        int total = m + n;
-
-        // Merge nums2 into nums1
-        nums1.resize(m + n);  // Make space for merged array
-        int i = m - 1;        // Pointer for nums1 original elements
-        int j = n - 1;        // Pointer for nums2
-        int k = m + n - 1;    // Pointer for merged nums1
-
-        // Merge from back to front
-        while (i >= 0 && j >= 0) {
-            if (nums1[i] > nums2[j]) {
-                nums1[k--] = nums1[i--];
+    vector<int> merge_array(vector<int>& a, vector<int>& b) {
+        int m = a.size();
+        int n = b.size();
+        vector<int> res;
+        int i = 0, j = 0;
+        while (i < m && j < n) {
+            if (a[i] <= b[j]) {
+                res.push_back(a[i]);
+                i++;
             } else {
-                nums1[k--] = nums2[j--];
+                res.push_back(b[j]);
+                j++;
             }
         }
 
-        // If any elements left in nums2
-        while (j >= 0) {
-            nums1[k--] = nums2[j--];
+        while (i < m) {
+            res.push_back(a[i]);
+            i++;
+        }
+        while (j < n) {
+            res.push_back(b[j]);
+            j++;
         }
 
-        // Now find the median
-        if (total % 2 == 0) {
-            // Even number of elements — average of two middle values
-            return (nums1[total / 2 - 1] + nums1[total / 2]) / 2.0;
-        } else {
-            // Odd number of elements — middle value
-            return nums1[total / 2];
-        }
+        return res;
+    }
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+
+        vector<int>res=merge_array(nums1,nums2);
+
+        int n=res.size();
+        
+        if(n%2==0){
+            double sum=res[n/2]+res[n/2-1];
+            return sum/2.00;
+        }  
+        else{
+            return res[n/2]*1.00;
+        }  
     }
 };
