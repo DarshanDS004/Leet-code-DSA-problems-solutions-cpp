fbb98907a1964c294325f1d97d142b2cca008883
DarshanDS004 <1si23is020@sit.ac.in>
2026-06-22 09:32:50 +0530

    Time: 4 ms (9.51%), Space: 10.6 MB (78.72%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,33 +1,21 @@
 class Solution {
 public:
-    string decimal_to_binary(int n){
-        string res="";
-        while(n>0){
-            res+=n%2+'0';
-            n/=2;
-        }
-        return res;
-    }
-
-    int  count_bits(string s){
-         int count=0;
-         for(int i=0;i<s.size();i++){
-            if(s[i]=='1'){
-                count++;
-            }
-
-         }
-         return count;
-    }
     vector<int> countBits(int n) {
-        vector<int>res;
-        for(int i=0;i<=n;i++){
-            string ans=decimal_to_binary(i);
-            int num=count_bits(ans);
-            res.push_back(num);
+        vector<int>res(n+1);
+        res[0]=0;
+        for(int i=1;i<=n;i++){
+            int count=0;
+            int p=i;
+            while(p){
+                if(p&1)
+                count++;
+
+                p>>=1;
+
+            }
+           res[i]=count;
         }
         return res;
-
         
     }
 };
