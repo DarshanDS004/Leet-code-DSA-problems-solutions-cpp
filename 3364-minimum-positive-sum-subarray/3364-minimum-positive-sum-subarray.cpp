c71665b551d720efc8cc9750208fca60aebd1141
DarshanDS004 <1si23is020@sit.ac.in>
2026-06-30 21:45:21 +0530

    Time: 0 ms (100%), Space: 29.5 MB (44.5%) - LeetHub

--- 3364-minimum-positive-sum-subarray/3364-minimum-positive-sum-subarray.cpp
+++ 3364-minimum-positive-sum-subarray/3364-minimum-positive-sum-subarray.cpp
@@ -1,68 +1,35 @@
-/*
 class Solution {
 public:
     int minimumSumSubarray(vector<int>& nums, int l, int r) {
         int n=nums.size();
-        int left=0;
-        int right=0;
-        int ans=INT_MAX;
-        
 
-        for(int i=l;i<=r;i++){
-             int sum=0;
-            for(int i=0;i<l;i++){
+        int ans=INT_MAX;
+          for(int len=l;len<=r;len++){
+
+            int sum=0;
+
+            for(int i=0;i<len;i++){
                 sum+=nums[i];
             }
 
             if(sum>0){
-                ans=min(ans,sum);
+               ans=min(ans,sum);
 
             }
 
-            for(int i=l;i<n;i++){
-                sum+=nums[i];
-                sum-=nums[i-l];
+            for(int j=len;j<n;j++){
+                sum+=nums[j];
+                sum-=nums[j-len];
 
                 if(sum>0){
                     ans=min(ans,sum);
                 }
             }
-        }
-        if(ans==INT_MAX)
-        return -1;
-        else 
-        return ans;
+          }
+          if(ans==INT_MAX)
+          return -1;
+          else
+          return ans;
         
-    }
-};
-
-*/
-
-class Solution {
-public:
-    int minimumSumSubarray(vector<int>& nums, int l, int r) {
-        int n = nums.size();
-        int ans = INT_MAX;
-
-        for (int len = l; len <= r; len++) {
-            int sum = 0;
-
-          
-            for (int i = 0; i < len; i++)
-                sum += nums[i];
-
-            if (sum > 0)
-                ans = min(ans, sum);
-
-            for (int i = len; i < n; i++) {
-                sum += nums[i];
-                sum -= nums[i - len];
-
-                if (sum > 0)
-                    ans = min(ans, sum);
-            }
-        }
-
-        return ans == INT_MAX ? -1 : ans;
     }
 };
