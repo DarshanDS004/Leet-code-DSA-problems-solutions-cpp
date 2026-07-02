64e3efcd8dc579f552e0e1e2c6c1886792548dcf
DarshanDS004 <1si23is020@sit.ac.in>
2026-07-02 10:30:50 +0530

    Time: 84 ms (23.31%), Space: 93.7 MB (89.27%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,17 +1,39 @@
+
+/*
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-       int n=nums.size();
-       unordered_map<int,int>mp;
-       for(int i=0;i<n;i++){
-        if(mp.count(nums[i]) && mp[nums[i]]!=i && abs(mp[nums[i]]-i)<=k){
-            return true;
+        int n = nums.size();
+        unordered_map<int, int> mp;
+        for (int i = 0; i < n; i++) {
+            if (mp.count(nums[i]) && mp[nums[i]] != i &&
+                abs(mp[nums[i]] - i) <= k) {
+                return true;
+            }
+            mp[nums[i]] = i;
         }
-        mp[nums[i]]=i;
-       }
 
+        return false;
+    }
+};
+*/
 
-      return false;
-        
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        int n=nums.size();
+        unordered_set<int>st;
+
+        for(int i=0;i<n;i++){
+            if(st.count(nums[i])){
+                return true;
+            }
+            st.insert(nums[i]);
+
+            if(st.size()>k)
+            st.erase(nums[i-k]);
+        }
+        return false;
+    
     }
 };
