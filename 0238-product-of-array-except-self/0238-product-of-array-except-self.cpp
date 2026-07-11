1b97bb6374d55e7cb423d9f72765941a3b19cc1f
DarshanDS004 <1si23is020@sit.ac.in>
2026-07-11 21:44:56 +0530

    Time: 1 ms (50.15%), Space: 40.2 MB (81.27%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,123 +1,43 @@
 
-
-// Brute Force Working but TLE 
 /*
 class Solution {
 public:
- int forward(int p,int size,vector<int>&nums)
- { 
-    int product=1;
-    for(int i=p+1;i<size;i++)
-    {
-        product*=nums[i];
-    }
-    return product;
- }
-
-
-int backward(int p,int size,vector<int>&nums)
- { 
-    int product=1;
-    for(int i=p-1;i>=0;i--)
-    {
-        product*=nums[i];
-    }
-    return product;
- }
-
-
     vector<int> productExceptSelf(vector<int>& nums) {
-       int n=nums.size();
-       vector<int>res(n);
-       for(int i=0;i<n;i++)
-       {
-           int p1=forward(i,n,nums);
-           int p2=backward(i,n,nums);
-           int p=p1*p2;
-           res[i]=p;
+        int n=nums.size();
+        vector<int>res;
 
-       }
-       return res;
+        for(int i=0;i<n;i++){
+            int product=1;
 
-        
+            for(int j=0;j<n;j++){
+                if(i!=j) {
+                    product*=nums[j];
+                }
+            }
+            res.push_back(product);
+        }    
+        return res;   
     }
 };
+
 */
 
-/*
 class Solution {
 public:
-vector<int> productExceptSelf(vector<int>& nums) {
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>res(n);
+        res[0]=1;
 
-         int n=nums.size();
-         vector<int>res(n,1);
-         int prefix=1;
-         for(int i=0;i<n;i++)
-         {
-                res[i]=prefix;
-                prefix*=nums[i];
-         }
-
-
+        for(int i=1;i<n;i++){
+            res[i]=res[i-1]*nums[i-1];
+        }
          int suffix=1;
-         for(int i=n-1;i>=0;i--)
-         {
+
+         for(int i=n-1;i>=0;i--){
             res[i]*=suffix;
             suffix*=nums[i];
-
          }
          return res;
-   }
-};
-*/
-
-
-/*
-class Solution {
-public:
-vector<int> productExceptSelf(vector<int>& nums) {
-     int n = nums.size();
-        vector<int> res(n);
-
-        for (int i = 0; i < n; i++) {
-            int product = 1;
-            for (int j = 0; j < n; j++) {
-                if (i != j) product *= nums[j];
-            }
-            res[i] = product;
-        }
-
-        return res;
-    }
-};
-
-
-*/
-
-#include <bits/stdc++.h>
-using namespace std;
-
-class Solution {
-public:
-    vector<int> productExceptSelf(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> prefix(n, 1), suffix(n, 1), res(n);
-
-        // Compute prefix products
-        for (int i = 1; i < n; i++) {
-            prefix[i] = prefix[i - 1] * nums[i - 1];
-        }
-
-        // Compute suffix products
-        for (int i = n - 2; i >= 0; i--) {
-            suffix[i] = suffix[i + 1] * nums[i + 1];
-        }
-
-        // Multiply prefix[i] * suffix[i]
-        for (int i = 0; i < n; i++) {
-            res[i] = prefix[i] * suffix[i];
-        }
-
-        return res;
     }
 };
