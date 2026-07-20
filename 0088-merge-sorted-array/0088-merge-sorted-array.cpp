73021e37202c736b77a98c1da411c88cc845beca
DarshanDS004 <1si23is020@sit.ac.in>
2026-07-20 07:49:55 +0530

    Time: 0 ms (100%), Space: 12.4 MB (15.19%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         int i=m-1,j=n-1,k=m+n-1;
-        // nums1.resize(m+n);
         while(i>=0&& j>=0){
             if(nums1[i]>nums2[j]){
                 nums1[k]=nums1[i];
@@ -16,22 +15,10 @@ public:
 
             k--;
         }
-
-      /*  while(i<m){
-            nums1[k--]=nums1[i--];
-            
-        }
-        */
         
         while(j>=0){
             nums1[k--]=nums2[j--];
         }
-       /*
-        nums1.resize(m+n);
-        for(int i=0;i<res.size();i++){
-            nums1[i]=res[i];
-        }
-        */
 
     }
 };
