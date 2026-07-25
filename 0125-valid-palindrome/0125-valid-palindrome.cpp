05d72317791e27bf1ce886f83be249d397cab9a8
DarshanDS004 <1si23is020@sit.ac.in>
2026-07-25 12:13:48 +0530

    Time: 0 ms (100%), Space: 10.1 MB (31.4%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
+  bool palindrome(string &s){
+    int left=0,right=s.size()-1;
+
+    while(left<right){
+        if(s[left]!=s[right]){
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+  }
     bool isPalindrome(string s) {
         int n=s.size();
-        int left=0,right=n-1;
-        while(left<right){
-
-            while(left <right  && !isalnum(s[left]))
-            left++;
-            while(left<right &&  !isalnum(s[right]))
-            right--;
-
-            if(tolower(s[left])!=tolower(s[right]))
-            return false;
-
-            left++;
-            right--;
+        string res="";
+        for(int i=0;i<n;i++){
+            if(isalnum(s[i])){
+                res+=tolower(s[i]);
+            }
         }
+       
+        
+       if(palindrome(res))
         return true;
+        else
+        return false;
         
     }
 };
