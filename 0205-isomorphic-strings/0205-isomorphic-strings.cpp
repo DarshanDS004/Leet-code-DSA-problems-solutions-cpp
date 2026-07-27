91c8b4883c88cb5ecb4172c64405e233480cff81
DarshanDS004 <1si23is020@sit.ac.in>
2026-07-27 10:14:50 +0530

    Time: 0 ms (100%), Space: 9.2 MB (82.65%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,3 +1,10 @@
+
+
+
+
+
+
+/*
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
@@ -24,5 +31,30 @@ public:
          }
          return true;
 
+    }
+};
+*/
+
+
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        int m=s.size();
+        int n=t.size();
+        if(m!=n)
+        return false;
+         int mp1[256]={0};
+         int mp2[256]={0};
+
+         for(int i=0;i<n;i++){
+            if(mp1[s[i]]!=mp2[t[i]]){
+                return false;
+            }
+
+            mp1[s[i]]=i+1;
+            mp2[t[i]]=i+1;
+         }
+         return true;
+        
     }
 };
