77890fe5f87406e46e38bf223a787fb6a78d216c
DarshanDS004 <1si23is020@sit.ac.in>
2026-07-27 15:23:03 +0530

    Time: 0 ms (100%), Space: 9.3 MB (68.51%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.cpp
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -1,54 +1,26 @@
-
-/*
 class Solution {
 public:
     int longestPalindrome(string s) {
+        int n=s.size();
         unordered_map<char,int>mp;
-        for(char ch:s)  // we can use vector as well 
-         mp[ch]++;
-         bool odd_found=false;
-         int length=0;
-        for(auto [key,value]:mp)
-        {
-            int count=value;
-            length+=(count/2)*2;
-            if(count%2==1)
-            {
-                odd_found=true;
+        for(char c:s){
+            mp[c]++;
+        }
+        bool hasOdd=false;
+        int ans=0;
+        for(auto [key,val]:mp){
+            if(val%2==0)
+            ans+=val;
+
+            else{
+                ans+=val-1;
+                hasOdd=true;
             }
         }
-        if(odd_found)
-        length++;
-
-        return length;
-
         
-    }
-};
-*/
-
-#include <bits/stdc++.h>
-using namespace std;
-
-class Solution {
-public:
-    int longestPalindrome(string s) {
-        vector<int> freq(128, 0); // ASCII size
-
-        for (char ch : s) {
-            freq[ch]++;
+        if(hasOdd){
+            ans++;
         }
-
-        int length = 0;
-        bool oddFound = false;
-
-        for (int count : freq) {
-            length += (count / 2) * 2;
-            if (count % 2 == 1) oddFound = true;
-        }
-
-        if (oddFound) length++;
-
-        return length;
+        return ans;
     }
 };
