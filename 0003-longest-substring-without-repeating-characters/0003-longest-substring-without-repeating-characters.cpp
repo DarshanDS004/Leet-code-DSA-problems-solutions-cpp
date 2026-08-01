2cf327286c6b656b25c3418e59f11a6e7de40436
DarshanDS004 <1si23is020@sit.ac.in>
2026-08-01 11:20:50 +0530

    Time: 7 ms (63.68%), Space: 19.2 MB (8.02%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,3 +1,5 @@
+
+/*
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
@@ -16,5 +18,25 @@ public:
         }
         return max_len;
         
+    }
+};
+*/
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n=s.size();
+        int left=0,right=0,max_len=0;
+        vector<int>last_idx(256,-1);
+        for(int  right=0;right<n;right++){
+             
+             if(last_idx[s[right]]>=left){
+                left=last_idx[s[right]]+1;
+             }
+
+             last_idx[s[right]]=right;
+             max_len=max(max_len,right-left+1);
+        }
+        return max_len;
     }
 };
