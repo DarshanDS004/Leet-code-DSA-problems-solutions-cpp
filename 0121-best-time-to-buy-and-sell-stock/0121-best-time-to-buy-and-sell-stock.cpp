5d77e445ab3b63253074de24e7f25bebcc2143a3
DarshanDS004 <1si23is020@sit.ac.in>
2026-08-01 16:18:19 +0530

    Time: 0 ms (100%), Space: 97.2 MB (88.08%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,17 +1,14 @@
-
 /*
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        int min_so_far=prices[0];
         int max_profit=0;
-
-        for(int i=1;i<n;i++){
-            if(prices[i]<min_so_far){
-                min_so_far=prices[i]; 
+        int n=prices.size();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int pro=prices[j]-prices[i];
+                max_profit=max(max_profit,pro);
             }
-            max_profit=max(max_profit,prices[i]-min_so_far);
         }
         return max_profit;
         
@@ -19,33 +16,19 @@ public:
 };
 */
 
-/*
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int n=prices.size();
         int max_profit=0;
+        int n=prices.size();
+        int min_so_far=INT_MAX;
         for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                max_profit=max(max_profit,prices[j]-prices[i]);
+            if(prices[i]<min_so_far){
+              min_so_far=prices[i];
             }
+            max_profit=max(max_profit,prices[i]-min_so_far);
         }
         return max_profit;
     }
 };
-*/
 
-class Solution {
-public:
-    int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        int max_profit=0;
-        int curr_profit=0;
-        for(int i=1;i<n;i++){  
-            curr_profit+=prices[i]-prices[i-1];
-            curr_profit=max(0,curr_profit);
-            max_profit=max(curr_profit,max_profit);
-        }
-        return  max_profit;
-    }
-};
