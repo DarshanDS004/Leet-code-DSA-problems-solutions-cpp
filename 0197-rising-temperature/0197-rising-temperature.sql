6f2681d9ab66c8d35effbb3cc196e5115dcb0736
DarshanDS004 <1si23is020@sit.ac.in>
2026-08-03 06:42:53 +0530

    Time: 468 ms (75.17%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,8 +1,12 @@
 # Write your MySQL query statement below
-select w.id
-from weather w
-join weather w1
-on datediff(w.recordDate,w1.recordDate)=1
-WHERE w.temperature>w1.temperature;
-
-
+select id 
+from (
+    select w.id,
+    w.recordDate,
+    w.temperature,
+    lag(w.recordDate)over(order by w.recordDate) as  prevDate,
+    lag(w.temperature)over(order by w.recordDate)as prevTemp
+    From weather w
+  ) temp
+  where datediff(recordDate, prevDate)=1
+  and temperature>prevTemp;
