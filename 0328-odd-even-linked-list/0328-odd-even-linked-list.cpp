aff2a07445f31206305f77cda2ba62b2bb15ce0d
DarshanDS004 <1si23is020@sit.ac.in>
2026-08-16 20:48:13 +0530

    Time: 0 ms (100%), Space: 15.6 MB (28.87%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -1,64 +1,33 @@
-/*
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        if (head == nullptr) return head;
-
-        vector<int> arr;
-
-        ListNode* temp = head;
-        int index = 1;
-
-        
-        while (temp != nullptr) {
-            if (index % 2 != 0) {
-                arr.push_back(temp->val);
-            }
-            temp = temp->next;
-            index++;
-        }
-
-     
-        temp = head;
-        index = 1;
-        while (temp != nullptr) {
-            if (index % 2 == 0) {
-                arr.push_back(temp->val);
-            }
-            temp = temp->next;
-            index++;
-        }
-
-        temp = head;
-        int i = 0;
-        while (temp != nullptr) {
-            temp->val = arr[i++];
-            temp = temp->next;
-        }
-
+        if(head==nullptr||head->next==nullptr)
         return head;
-    }
-};
-*/
-
-class Solution {
-public:
-    ListNode* oddEvenList(ListNode* head) {
-        if (head == nullptr) return head;
-        ListNode*temp=head;
         ListNode*odd=head;
         ListNode*even=head->next;
         ListNode*evenhead=even;
 
-        while(even!=nullptr &&even->next!=nullptr)
-        {
+        while(even!=nullptr && even->next!=nullptr){
             odd->next=even->next;
             odd=odd->next;
+
             even->next=odd->next;
             even=even->next;
         }
-        
         odd->next=evenhead;
+
         return head;
+        
+        
     }
 };
