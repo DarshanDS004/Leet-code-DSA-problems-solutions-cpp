58717fac7ffc137664b411150f92101c24a6eb51
DarshanDS004 <1si23is020@sit.ac.in>
2026-08-19 20:50:21 +0530

    Time: 0 ms (100%), Space: 15.3 MB (6.61%) - LeetHub

--- 0867-transpose-matrix/0867-transpose-matrix.cpp
+++ 0867-transpose-matrix/0867-transpose-matrix.cpp
@@ -4,14 +4,13 @@ public:
         int n=matrix.size();
         int m=matrix[0].size();
         vector<vector<int>>res(m,vector<int>(n));
+        
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-               res[j][i]=matrix[i][j];
+                 res[j][i]=matrix[i][j];
             }
         }
         return res;
-
-
         
     }
 };
