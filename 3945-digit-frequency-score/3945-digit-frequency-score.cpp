4f9932d428c04fabe2be2723720d85ab5675b885
DarshanDS004 <1si23is020@sit.ac.in>
2026-08-24 07:41:43 +0530

    Time: 0 ms (100%), Space: 9.1 MB (37.85%) - LeetHub

--- 3945-digit-frequency-score/3945-digit-frequency-score.cpp
+++ 3945-digit-frequency-score/3945-digit-frequency-score.cpp
@@ -2,7 +2,9 @@ class Solution {
 public:
     int digitFrequencyScore(int n) {
         unordered_map<int,int>mp;
-        
+        if (n == 0) {
+            mp[0] = 1;
+        }
         
         while(n!=0){
             mp[n%10]++;
