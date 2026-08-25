025b022393d494e59541d2c2fe82e56343e443c8
DarshanDS004 <1si23is020@sit.ac.in>
2026-08-25 07:30:59 +0530

    Time: 7 ms (9.55%), Space: 25.2 MB (24.36%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
@@ -1,3 +1,5 @@
+
+/*
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
@@ -13,5 +15,20 @@ public:
         }
         return mul;
         
+    }
+};
+*/
+class Solution {
+public:
+    int missingMultiple(vector<int>& nums, int k) {
+        unordered_set<int> st(nums.begin(), nums.end());
+
+        int mul = k;
+
+        while (st.count(mul)) {
+            mul += k;
+        }
+
+        return mul;
     }
 };
