5aae0c427f22d5a91c094f70eb5124ec36c372d0
DarshanDS004 <1si23is020@sit.ac.in>
2026-09-26 12:06:17 +0530

    Time: 51 ms (41.04%), Space: 29.1 MB (45.53%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,43 +1,44 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
-        vector<vector<int>>res;
-        for(int i=0;i<n;i++){
-            if(i>0 && nums[i]==nums[i-1])
-            continue;
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
 
-            int left=i+1;
-            int right=n-1;
+        vector<vector<int>> res;
 
-            while(left<right){
-                int sum=nums[i]+nums[left]+nums[right];
+        for (int i = 0; i < n; i++) {
 
-                if(sum==0){
-                    res.push_back({nums[i],nums[left],nums[right]});
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    res.push_back({nums[i], nums[left], nums[right]});
+
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+
+                    while (left < right && nums[right] == nums[right -1]) {
+                        right--;
+                    }
+
                     left++;
                     right--;
-
-                    
-                while(left<right && nums[left]==nums[left-1])
-                left++;
-
-                while(left<right && nums[right]==nums[right+1])
-                right--;
-              }
-                else if(sum<0){
-                    left++;
                 }
-                else{
+
+                else if (sum < 0) {
+                    left++;
+                } else {
                     right--;
                 }
-
-               
             }
         }
-        return res;
-
-        
-    }
-};
+            return res;
+        }
+    };
